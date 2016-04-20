b407d54720edd782fa7b59f5718250e0be9ccf4a
kennp174 <PhoebeKennedy@Phoebes-MacBook-Air-2.local>
2016-04-19 23:43:15 -0400

    weather

--- sketch.js
+++ sketch.js
@@ -1,26 +1,85 @@
 var cities;
 var city;
+var cloud;
+var rain;
+var mist;
+var temp;
+var windy;
+//var circleX =0;
+
+
+
+
+
+function preload(){
+  cloud = loadImage('img/clouds.jpg');
+  rain = loadImage('img/rain.jpg');
+  clear = loadImage('img/clearsky.jpg');
+  windy = loadImage('img/windy.png');
+}
+
 
 function setup() {
 
   cities = createSelect();
   cities.option('New York,NY');
-  cities.option('Miami, FL');
+  cities.option('Dallas, TX');
+  cities.option('Charleston, SC');
+  cities.option('Seattle, WA');
+  cities.option('Cincinnatti, OH');
+
+
   //Change these cities and/or add your own
 
   cities.changed(loadCity);
   loadCity();
 
-  createCanvas(400,400);
+  createCanvas(1000,800);
 
 }
 
 function draw() {
-  if(city){ //Your drawing should go inside this if statement
+  //fill("red");
+  //ellipse (circleX, 100,50,50);
+  //circleX = circleX +5;
+  if(city){
+    background("red");
+    if (city.weather[0].main == "Clear"){
+    image (clear,0, 0, background.width, background.height)
 
-    background("grey");
-    text(city.name,50,50); //feel free to delete this
+    } else if (city.weather[0].main == "Clouds"){
+      image (cloud, 0, 0, background.width, background.height)
+    } else if (city.weather[0].main == "Rain"){
+      image (rain, 0, 0, background.width, background.height)
+    } else if (city.weather[0].main == "Windy"){
+      background("blue");
+      image (windy, mouseX, mouseY, 200, 200);
+    }
+      //else if (circleX >= 1000) {
+      //circleX=0;
+      //}
 
+
+
+
+    fill(floor(city.main.temp*3), floor(city.main.temp*3),0);
+    noStroke();
+    ellipse(900,100,150,150);
+
+
+
+    var sunset = new Date(city.sys.sunset*1000);
+    textSize(50);
+    fill('red');
+    text(sunset.toLocaleTimeString(),325,700);
+    textSize(30);
+    fill("white");
+    text(city.weather[0].main,20,40);
+
+    var temp = city.main.temp;
+    fill ("blue");
+    textSize(40);
+    text(temp,850,80,200,200);
 
   }
 }
